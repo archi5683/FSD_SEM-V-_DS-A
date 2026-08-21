dff0a34bf560a284056fdc2909f457275bd33be8
archi5683 <archigupta5683@gmail.com>
2026-08-21 10:30:56 +0530

    Callback Hell in js

---
+++ JavaScript/callback_hell.js
@@ -0,0 +1,14 @@
+function sum(a, b, callback) {
+    let result = a + b;
+    callback(result);
+}
+
+function multiply(a, b, callback) {
+    let result = a * b;
+    callback(result);
+}
+
+function subtract(a, b, callback) {
+    let result = a - b;
+    callback(result);
+}
